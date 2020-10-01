d8304b6b2934d5568e8939471ccab3edf7423300
jagu4r <omar.mendrade@gmail.com>
2020-10-01 12:24:30 -0500

    first commit

---
+++ functions.py
@@ -0,0 +1,14 @@
+##=============================================================================
+## CREDENTIALS
+##=============================================================================
+### These are my personal twitter developer keys to scrap into twitter and get 
+### some tweets. 
+##=============================================================================
+def credentials():
+    consumer_key = 'q24kCMia4SqAPJDUVMJ21ad1n',
+    consumer_secret = 'hw4fXnivIuT7ssHX3GfTorFtzlA4xwuTuIqLudTHz1y6yFamUh',
+    access_token = '1116162409159327745-Y3wDT7tQgJ5rqHWw84ERz3o8yZXM7k',
+    access_token_secret = 'SfXJEvvrOxonxRgnafCKFT8f5xeDlbUC11YIzOfJpqPvr'
+    return consumer_key, consumer_secret, access_token, access_token_secret
+
+
